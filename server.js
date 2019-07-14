909b03ffda77d4cad82eee0dc497e6fa917a016e
chknwaffles <wonsae.kim@gmail.com>
2019-07-14 03:07:40 -0400

    websockets way

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const app = require('express')()
 const server = require('http').createServer(app)
-const io = require('socket.io')(server)
+const WebSocket = require('ws')
 const port = process.env.PORT || 3000
 const cors = require('cors')
 
 app.use(cors())
-  
-io.on('connection', socket => {
-    socket.on('message', data => {
-        socket.broadcast.emit('received', { data })
-        console.log(data)
+
+const wss = new WebSocket.Server({ port: port })
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(data) {
+        wss.clients.forEach(function each(client) {
+            if (client !== ws && client.readyState == WebSocket.OPEN) {
+                client.send(data)
+                console.log(data)
+            }
+        })
     })
 })
 
